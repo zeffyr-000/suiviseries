e7a0a4feab0e7129b031940d8102bc8d30ffd1fe
zeffyr-000 <sparkman000@hotmail.com>
2026-01-11 01:19:40 +0100

    bugs on backdrops

--- src/app/serie-detail/serie-images/serie-images.component.spec.ts
+++ src/app/serie-detail/serie-images/serie-images.component.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect, beforeEach } from 'vitest';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SerieImagesComponent } from './serie-images.component';
 import { SerieImages } from '../../models/serie.model';
